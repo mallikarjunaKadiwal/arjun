d8ac3785583bcbd23361e4aacc7b2f4ecb801e82
mallikarjunaKadiwal <mallikarjunakadiwal86@gmail.com>
2026-01-21 20:53:35 +0530

    2026-01-21T20:53:35+05:30

--- index.js
+++ index.js
@@ -5,35 +5,22 @@ import random from "random";
 
 const path = "./data.json";
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
-
-  const data = {
-    date: date,
-  };
-
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
-  });
-};
-
 const makeCommits = (n) => {
   if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
+  
+  const x = random.int(0, 12);
   const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  
+  const date = moment().subtract(x, "w").subtract(y, "d").format();
 
   const data = {
     date: date,
   };
+  
   console.log(date);
+  
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+    simpleGit().add([path]).commit(date, { "--date": date }, makeCommits.bind(this, --n));
   });
 };
 
